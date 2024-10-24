cca416c333c9245f5954bf8338813fcbee3704d8
Sharon Kirimi <kirimisharon@gmail.com>
2024-10-24 14:25:12 +0200

    First Commit (Draft)

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Dark Mode Toggle
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
